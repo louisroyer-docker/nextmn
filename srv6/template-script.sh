90f1e43bd0611448e0fe388d5cd110359cfd9246
Louis Royer <infos.louis.royer@gmail.com>
2024-02-01 17:20:18 +0100

    Some environment variables are now optional in nextmn-srv6

--- srv6/template-script.sh
+++ srv6/template-script.sh
@@ -11,24 +11,12 @@ if [ -z "$HOOKS" ]; then
 	echo "Missing mandatory environment variable (HOOKS)." > /dev/stderr
 	exit 1
 fi
-if [ -z "$HEADENDS" ]; then
-	echo "Missing mandatory environment variable (HEADENDS)." > /dev/stderr
-	exit 1
-fi
 if [ -z "$ENDPOINTS" ]; then
 	echo "Missing mandatory environment variable (ENDPOINTS)." > /dev/stderr
 	exit 1
 fi
 if [ -z "$LOCATOR" ]; then
 	echo "Missing mandatory environment variable (LOCATOR)." > /dev/stderr
-	exit 1
-fi
-if [ -z "$LINUX_HEADEND_SET_SOURCE_ADDRESS" ]; then
-	echo "Missing mandatory environment variable (LINUX_HEADEND_SET_SOURCE_ADDRESS)." > /dev/stderr
-	exit 1
-fi
-if [ -z "$IPV4_HEADEND_PREFIX" ]; then
-	echo "Missing mandatory environment variable (IPV4_HEADEND_PREFIX)." > /dev/stderr
 	exit 1
 fi
 
@@ -39,16 +27,29 @@ for HOOKS in ${HOOKS}; do
 		HOOKS_SUB="${HOOKS_SUB}\n  ${HOOK}"
 	fi
 done
-for HEADENDS in ${HEADENDS}; do
-	if [ -n "${HEADEND}" ]; then
-		HEADENDS_SUB="${HEADENDS_SUB}\n  ${HEADEND}"
-	fi
-done
+
+if [ -n "${HEADENDS}" ]; then
+	HEADENDS_SUB="headends: "
+	for HEADENDS in ${HEADENDS}; do
+		if [ -n "${HEADEND}" ]; then
+			HEADENDS_SUB="${HEADENDS_SUB}\n  ${HEADEND}"
+		fi
+	done
+fi
+
+ENDPOINTS_SUB=""
 for ENDPOINTS in ${ENDPOINTS}; do
 	if [ -n "${ENDPOINT}" ]; then
 		ENDPOINTS_SUB="${ENDPOINTS_SUB}\n  ${ENDPOINT}"
 	fi
 done
+
+if [ -n "$LINUX_HEADEND_SET_SOURCE_ADDRESS" ]; then
+	LINUX_HEADEND_SET_SOURCE_ADDRESS_SUB="linux-headend-set-source-address: ${LINUX_HEADEND_SET_SOURCE_ADDRESS}"
+fi
+if [ -n "$IPV4_HEADEND_PREFIX" ]; then
+	IPV4_HEADEND_PREFIX_SUB="ipv4-headend-prefix: ${IPV4_HEADEND_PREFIX}"
+fi
 
 awk \
 	-v DEBUG="${DEBUG:-false}" \
@@ -56,8 +57,8 @@ awk \
 	-v HEADENDS="${HEADENDS_SUB}" \
 	-v ENDPOINTS="${ENDPOINTS_SUB}" \
 	-v LOCATOR="${LOCATOR}" \
-	-v IPV4_HEADEND_PREFIX="${IPV4_HEADEND_PREFIX}" \
-	-v LINUX_HEADEND_SET_SOURCE_ADDRESS="${LINUX_HEADEND_SET_SOURCE_ADDRESS}" \
+	-v IPV4_HEADEND_PREFIX="${IPV4_HEADEND_PREFIX_SUB}" \
+	-v LINUX_HEADEND_SET_SOURCE_ADDRESS="${LINUX_HEADEND_SET_SOURCE_ADDRESS_SUB}" \
 	'{
 		sub(/%DEBUG/, DEBUG);
 		sub(/%HOOKS/, HOOKS);
