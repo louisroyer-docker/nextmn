9ef0b153e26c266cb41b067b63fc9fc4d2486ac3
Louis Royer <infos.louis.royer@gmail.com>
2024-07-17 20:41:19 +0200

    upgrade upf to v0.0.7

--- upf/template-script.sh
+++ upf/template-script.sh
@@ -15,15 +15,10 @@ if [ -z "$DNN_LIST" ]; then
 	echo "Missing mandatory environment variable (DNN_LIST)." > /dev/stderr
 	exit 1
 fi
-if [ -z "$GTPU_ENTITIES_LIST" ]; then
-	echo "Missing mandatory environment variable (GTPU_ENTITIES_LIST)." > /dev/stderr
+if [ -z "$IF_LIST" ]; then
+	echo "Missing mandatory environment variable (IF_LIST)." > /dev/stderr
 	exit 1
 fi
-# For future: https://github.com/nextmn/upf/issues/30
-#if [ -z "$IF_LIST" ]; then
-#	echo "Missing mandatory environment variable (IF_LIST)." > /dev/stderr
-#	exit 1
-#fi
 
 IFS=$'\n'
 
@@ -33,28 +28,21 @@ for DNN in ${DNN_LIST}; do
 		DNN_LIST_SUB="${DNN_LIST_SUB}\n  ${DNN}"
 	fi
 done
-# For future: https://github.com/nextmn/upf/issues/30
-#IF_LIST_SUB=""
-#for INTERFACE in ${IF_LIST}; do
-#	if [ -n "${INTERFACE}" ]; then
-#		IF_LIST_SUB="${IF_LIST_SUB}\n    ${INTERFACE}"
-#	fi
-#done
-GTPU_ENTITIES_LIST_SUB=""
-for GTPU_ENTITIES in ${GTPU_ENTITIES_LIST}; do
-	if [ -n "${GTPU_ENTITIES}" ]; then
-		GTPU_ENTITIES_LIST_SUB="${GTPU_ENTITIES_LIST_SUB}\n  ${GTPU_ENTITIES}"
+IF_LIST_SUB=""
+for INTERFACE in ${IF_LIST}; do
+	if [ -n "${INTERFACE}" ]; then
+		IF_LIST_SUB="${IF_LIST_SUB}\n    ${INTERFACE}"
 	fi
 done
 
 awk \
 	-v N4="${N4}" \
 	-v DNN_LIST="${DNN_LIST_SUB}" \
-	-v GTPU_ENTITIES_LIST="${GTPU_ENTITIES_LIST_SUB}" \
+	-v IF_LIST="${IF_LIST_SUB}" \
 	'{
 		sub(/%N4/, N4);
 		sub(/%DNN_LIST/, DNN_LIST);
-		sub(/%GTPU_ENTITIES_LIST/, GTPU_ENTITIES_LIST);
+		sub(/%IF_LIST/, IF_LIST);
 		print;
 	}' \
 	"${CONFIG_TEMPLATE}" > "${CONFIG_FILE}"
